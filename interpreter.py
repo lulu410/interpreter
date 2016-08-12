08075f1b6d2cede55213170a1d983f9b416bdbe4
lulu410 <qx39@cornell.edu>
2016-08-12 16:30:29 -0400

    start implement a not-so-naive interpreter

--- interpreter.py
+++ interpreter.py
@@ -1,70 +1,33 @@
-#use a stack
-class Interpreter:
+class VirtualMachineError(Exception):
+	pass
+
+class VirtualMachine(object):
 	def __init__(self):
+		self.frames = []
+		self.frame = None
+		self.return_value = None
+		self.last_exception = None
+
+	def run(self, code, global_names=None, local_names=None):
+		frame = self.make_frame(code, global_names=global_names, 
+								local_names=local_names)
+
+	def make_frame(self, code, callargs={}, global_names=None, local_names=None)
+
+
+class Frame(object):
+	def __init__(self, code_obj, global_names, local_names, prev_frame):
+		self.code_obj = code_obj
+		self.global_names = global_names
+		self.local_names = local_names
+		self.prev_frame = prev_frame
 		self.stack = []
-		self.environment = {}
+		if prev_frame:
+			self.builtin_names = prev_frame.builtin_names
+		else:
+			self.builtin_names = local_names['__builtins__']
+			if hasattr(self.builtin_names, '__dict__'):
+				self.builtin_names = self.builtin_names.__dict__
 
-
-	def LOAD_VALUE(self, number):
-		self.stack.append(number)
-
-
-	def LOAD_VALUE(self,name):
-		val = environment[name]
-		self.stack.append(val)
-
-
-	def STORE_NAME(self, name):
-		val = self.stack.pop()
-		self.environment[name] = val
-
-	
-	def PRINT_ANSWER(self):
-		answer = self.stack.pop()
-		print answer
-
-
-	def ADD_TWO_VALUES(self):
-		num1 = self.stack.pop()
-		num2 = self.stack.pop()
-		answer = num1+num2
-		self.stack.append(answer)
-
-
-	def parse_argument(self, instr, arg, what_to_execute):
-		numbers = ["LOAD_VALUE"]
-		names = ["LOAD_VALUE", "STORE_NAME"]
-
-		if instruction in numbers:
-			arg = what_to_execute["numbers"][arg]
-		elif instruction in names:
-			arg = what_to_execute["names"][arg]
-
-		return arg
-
-
-	def run(self,what_to_execute):
-		instructions = what_to_execute["instructions"]
-		for instr, arg in instructions:
-			arg = self.parse_argument(instr, arg, what_to_execute)
-			if instr == "LOAD_VALUE":
-				self.LOAD_VALUE(num)
-			elif instr == "ADD_TWO_VALUES":
-				self.ADD_TWO_VALUES()
-			elif instr == "PRINT_ANSWER":
-				self.PRINT_ANSWER()
-			elif instr == "STORE_NAME":
-				self,STORE_NAME(arg)
-			elif instruction == "LOAD_VALUE":
-				self.LOAD_VALUE(arg)
-
-
-what_to_execute = {
-	"instructions": [("LOAD_VALUE", 0),  # the first number
-					 ("LOAD_VALUE", 1),  # the second number
-					 ("ADD_TWO_VALUES", None),
-			         ("PRINT_ANSWER", None)],
-	"numbers": [7, 5] }
-
-interpreter = Interpreter()
-interpreter.run(what_to_execute)
+		self.last_exception = 0
+		self.block_stack = []
